bb3aa6390e05d05820de2188ada84c02f88bf9e1
ClauderZ <mzcloud12@gmail.com>
2019-04-16 14:21:48 +0800

    Contributers

--- application/logs/log-2019-04-16.php
+++ application/logs/log-2019-04-16.php
@@ -15,3 +15,33 @@ ERROR - 2019-04-16 04:56:06 --> Query error: Access denied for user 'zeroafmn_ze
 	CONSTRAINT `pk_modules` PRIMARY KEY(`module_name`)
 ) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COLLATE = utf8_general_ci
 ERROR - 2019-04-16 04:56:06 --> Severity: Error --> Call to a member function real_escape_string() on a non-object C:\xampp\htdocs\discoverpalawan\system\database\drivers\mysqli\mysqli_driver.php 391
+ERROR - 2019-04-16 06:14:01 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'zeroafmn_discoverpalawan' C:\xampp\htdocs\discoverpalawan\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2019-04-16 06:14:01 --> Unable to connect to the database
+ERROR - 2019-04-16 06:14:01 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'zeroafmn_discoverpalawan' C:\xampp\htdocs\discoverpalawan\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2019-04-16 06:14:01 --> Unable to connect to the database
+ERROR - 2019-04-16 06:14:01 --> Query error: Unknown database 'zeroafmn_discoverpalawan' - Invalid query: CREATE TABLE IF NOT EXISTS `modules` (
+	`module_name` VARCHAR(60) NOT NULL,
+	`version_code` INT NOT NULL,
+	`version_name` VARCHAR(60) NOT NULL,
+	`_enabled` INT NOT NULL,
+	`_order` INT NOT NULL,
+	`updated_at` DATETIME NOT NULL,
+	`created_at` DATETIME NOT NULL,
+	CONSTRAINT `pk_modules` PRIMARY KEY(`module_name`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COLLATE = utf8_general_ci
+ERROR - 2019-04-16 06:14:01 --> Severity: Error --> Call to a member function real_escape_string() on a non-object C:\xampp\htdocs\discoverpalawan\system\database\drivers\mysqli\mysqli_driver.php 391
+ERROR - 2019-04-16 06:14:58 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'zeroafmn_discoverpalawan' C:\xampp\htdocs\discoverpalawan\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2019-04-16 06:14:58 --> Unable to connect to the database
+ERROR - 2019-04-16 06:14:58 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'zeroafmn_discoverpalawan' C:\xampp\htdocs\discoverpalawan\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2019-04-16 06:14:59 --> Unable to connect to the database
+ERROR - 2019-04-16 06:14:59 --> Query error: Unknown database 'zeroafmn_discoverpalawan' - Invalid query: CREATE TABLE IF NOT EXISTS `modules` (
+	`module_name` VARCHAR(60) NOT NULL,
+	`version_code` INT NOT NULL,
+	`version_name` VARCHAR(60) NOT NULL,
+	`_enabled` INT NOT NULL,
+	`_order` INT NOT NULL,
+	`updated_at` DATETIME NOT NULL,
+	`created_at` DATETIME NOT NULL,
+	CONSTRAINT `pk_modules` PRIMARY KEY(`module_name`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8 COLLATE = utf8_general_ci
+ERROR - 2019-04-16 06:14:59 --> Severity: Error --> Call to a member function real_escape_string() on a non-object C:\xampp\htdocs\discoverpalawan\system\database\drivers\mysqli\mysqli_driver.php 391
